af65280e736b85b84fafb0381ef45f99d9e3142c
Malyuta Aleksey <overby999@gmail.com>
2021-06-04 21:13:14 +0300

    04.06.21 21:13

--- src/pages/api/orders/index.js
+++ src/pages/api/orders/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default async (req, res) => {
+  console.log('-----', 'process.env.API', process.env.API);
   const response = await axios.post(`${process.env.API}/v1/transaction`, {
     shop_id: '66ac27bf-fce5-49ec-bb48-95688f6a131f',
     traditional_currency: {
